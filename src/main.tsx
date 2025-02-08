950e93223f8878de2f796d5c3522310e5f1aea95
Emanuel <emanuel.snasc1@gmail.com>
2025-02-08 18:56:36 -0300

    style: main style

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import Routes from "./routes";
+import "./main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
